a9cdf4202b601267e5e431f9d756a5251d07e842
VilleWalo <k.s.a1991@yandex.ru>
2016-05-31 18:49:17 +0300

    second

--- app/app.js
+++ app/app.js
@@ -13,8 +13,7 @@ var content = document.getElementById("content");
     (function loadCoords() {
       var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
       var xhr = new XHR();
-      // xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/weather?lat='+lat+'&lon='+lon+'&APPID={a21f1d86bc62d7031ed00a1225259126}', true);
-      xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/weather?lat=49.67&lon=58.60&APPID={a21f1d86bc62d7031ed00a1225259126}', true);
+      xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={a21f1d86bc62d7031ed00a1225259126}', true);
       xhr.onload = function() {
   		alert( this.responseText );
 	  }
@@ -27,6 +26,7 @@ var content = document.getElementById("content");
       // xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon, true);
       // xhr.send();
 // a21f1d86bc62d7031ed00a1225259126 Default
+//'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID={a21f1d86bc62d7031ed00a1225259126}
       // xhr.onreadystatechange = function() { 
       // if (xhr.readyState != 4) return;
       // button.innerHTML = 'Готово!';
